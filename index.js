c72048c43e1a0af5c054d32499dd5b2ad53cecce
José Moreira <jmoreira@microdual.com>
2015-04-03 03:02:24 +0100

    soneca

---
+++ index.js
@@ -0,0 +1,13 @@
+module.exports = exports = require( './lib/requirehit.js' );
+
+// Needed by our main engine (this repo)
+exports.Package = require( 'requirehit-package' );
+exports.Bundler = require( 'requirehit-bundler' );
+
+// Exposing part of our engine that is modulated
+exports.Browser = require( 'requirehit-browser' );
+
+// Extendable classes
+exports.Adapter = require( 'requirehit-adapter' );
+exports.Storage = require( 'requirehit-storage' );
+exports.Builder = require( 'requirehit-builder' );
